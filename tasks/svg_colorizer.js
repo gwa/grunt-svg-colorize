3b83047bc0d6f7fe512b1723bad6e5ceeb5438d1
Lauren Hamel <lauren.hamel@outlook.com>
2018-12-12 11:12:19 -0500

    Improves colorizer
    - Adds support for colors as array, object, or string
    - Adds `delimiter` option
    - Adds `silent` option for silencing console logging
    - Adds `subfolders` option for placing files in basenamed subfolders

--- tasks/svg_colorizer.js
+++ tasks/svg_colorizer.js
@@ -1,6 +1,6 @@
 /*
- * grunt-svg-colorize
- * https://github.com/gwa/grunt-svg-colorize
+ * grunt-svg-colorizer
+ * https://github.com/laurenhamel/grunt-svg-colorizer
  *
  * Copyright (c) 2017 Timothy Groves
  * Licensed under the MIT license.
@@ -14,50 +14,116 @@ module.exports = function(grunt) {
   // creation: http://gruntjs.com/creating-tasks
 
   grunt.registerMultiTask('svg_colorizer', 'colorize single color SVGs.', function() {
+    
+    // Initialize dependencies.
+    const type = require('get-type');
+    const path = require('path');
+    
     // Merge task-specific and/or target-specific options with these defaults.
     var options = this.options({
-      basecolor: '#000',
-      colors: []
+      delimiter: '-',
+      basecolor: '#000000',
+      colors: {},
+      silent: false,
+      subfolders: true
     });
+    
+    // Initialize utility methods.
+    const utils = {
+      
+      hexed: /^\#/,
+      
+      colorize: ( svg, hex ) => svg.replace(new RegExp(`#${options.basecolor.replace(utils.hexed, '')}`, 'g'), `#${hex.replace(utils.hexed, '')}`),
+      
+      path: ( basename, color ) => options.subfolders ? `${basename}/${utils.filename(basename, color)}` : utils.filename(basename, color),
+      
+      filename: ( basename, color ) => `${basename}${options.delimiter}${color}.svg`,
+      
+      convert( file, src, svg, color, hex ) {
+        
+        // Get the colorized version of the file.
+        const colorized = utils.colorize(svg, hex);
+
+        // Build the file's basename.
+        const basename = path.basename(src, '.svg');
+
+        // Generate the new file path.
+        const dest = utils.path(basename, color);
+
+        // Save the file.
+        utils.save(file, dest, colorized);
+        
+      },
+      
+      save ( file, src, data ) {
+        
+        // Get the destination path.
+        const dest = (path.extname(file.dest) ? path.dirname(file.dest) : file.dest).replace(/\/$/, '');
+        
+        // Save the file.
+        grunt.file.write(`${dest}/${src}`, data);
+        
+        // Log a success message.
+        if( !options.silent ) grunt.log.writeln(`File '${src}' created.`);
+        
+      }
+      
+    };
 
     // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
+    this.files.forEach((file) => {
+      
+      // Concatenate specified files.
+      file.src.filter((src) => {
+        
+        // Warn about invalid source files, and then ignore them.
+        if ( !grunt.file.exists(src) ) {
+          
+          grunt.log.warn(`Source file '${src}' not found.`);
+          
           return false;
+          
         }
 
+        // Otherwise, use valid source files.
         return true;
-      }).map(function(filepath) {
-        // Read file source.
-        var src = grunt.file.read(filepath);
-
-        options.colors.forEach(function(color) {
-          var colorized_src = colorize(src, options.basecolor, color),
-            filename = filepath.substring(filepath.lastIndexOf('/') + 1),
-            newpath = f.dest + '/' + getNewFileName(filename, color);
-
-          grunt.file.write(newpath, colorized_src);
-          grunt.log.writeln('File "' + newpath + '" created.');
-        });
+      
+      })
+      
+      // Process each group of files.
+      .map((src) => {
+      
+        // Read the source file.
+        const svg = grunt.file.read(src);
+        
+        // Handle color arrays.
+        if( type.isArray(options.colors) ) {
+          
+          // Convert the SVG to the each color.
+          options.colors.forEach((color) => utils.convert(file, src, svg, color, color));
+          
+        }
+        
+        // Handle color objects.
+        else if( type.isObject(options.colors) ) {
+          
+          // Convert the SVG to each color.
+          for( let color in options.colors ) { utils.convert(file, src, svg, color, options.colors[color]); }
+          
+        }
+        
+        // Handle color strings.
+        else if( type.isString(options.colors) ) {
+          
+          // Convert the SVG to the given color.
+          utils.convert(file, src, svg, options.colors, options.colors);
+          
+        }
+        
       });
 
-      // Write the destination file.
-      //grunt.file.write(f.dest, src);
-
-      // Print a success message.
     });
+    
   });
 
 };
-
-function getNewFileName(filename, color) {
-  return filename.replace('.svg', '-' + color + '.svg');
-}
-
-function colorize(svg, basecolor, hexcolor) {
-  return svg.replace(new RegExp(basecolor, 'g'), '#' + hexcolor);
-}
